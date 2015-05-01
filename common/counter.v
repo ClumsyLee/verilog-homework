ec2e9db71f78ab366b2c0ffee41a73564b8ab168
Thomas Lee <lisihan969@gmail.com>
2015-05-02 02:21:15 +0800

    Modify the behavior of counter

--- common/counter.v
+++ common/counter.v
@@ -1,7 +1,7 @@
 module counter(num, signal, enable, reset_n);
 
 parameter WIDTH = 4,
-          MAX = 1 << WIDTH;
+          MAX = (1 << WIDTH) - 1;
 
 output [WIDTH - 1:0] num;
 input signal, enable, reset_n;
@@ -12,7 +12,10 @@ always @(posedge enabled_signal or negedge reset_n) begin
     if(~reset_n) begin
         num <= 0;
     end else begin
-        num <= num + 1'b1;
+        if (num >= MAX)
+            num <= 0
+        else
+            num <= num + 1'b1;
     end
 end
 
