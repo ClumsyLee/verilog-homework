1055dfbc410c98eb5009bdc74313fc83f8ffdc8c
Thomas Lee <lisihan969@gmail.com>
2015-05-02 15:25:47 +0800

    Implement most of exp3

--- exp3/freq_meter/freq_meter.v
+++ exp3/freq_meter/freq_meter.v
@@ -15,9 +15,27 @@ output range_now = range;
 input signal, range, clk;
 
 wire new_signal;
-
 divider divider1(new_signal, range, signal);
 
+// Build internal clocks.
+wire control_clk, scan_clk;
+watchmaker #(100_000_000) control_watchmaker(control_clk, clk);
+watchmaker #(100_000) scan_watchmaker(scan_clk, clk);
 
+// Central control.
+wire [15:0] num;
+reg [15:0] led_num;  // Number to be displayed on the LED.
+reg reset_n;
+counter #(16, 9999) four_bit_counter(num, new_signal, 1'b1, reset_n);
+
+// Reset counter on control_clk
+always @(posedge control_clk) begin
+    led_num <= num;
+    reset_n <= 0
+    @(posedge clk) reset_n <= 1
+end
+
+// Display the number
+led led1(anodes, cathodes, num, scan_clk);
 
 endmodule
