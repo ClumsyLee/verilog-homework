867b55b42f484651468fdb2255c9e64239f22189
Thomas Lee <lisihan969@gmail.com>
2015-05-03 09:53:47 +0800

    Add reset_n in watchmaker

--- common/watchmaker.v
+++ common/watchmaker.v
@@ -1,14 +1,18 @@
-module watchmaker(new_clk, clk);
+module watchmaker(new_clk, clk, reset_n);
 
 output reg new_clk;
-input clk;
+input clk, reset_n;
 
-parameter RATIO = 100_000_000;  // V10 = 10MHz
+parameter RATIO = 100_000_000,  // V10 = 10MHz
+          HALF_RATIO = RATIO / 2;
 
 integer counter;
 
-always @(clk) begin
-    if (counter >= RATIO) begin
+always @(posedge clk, negedge reset_n) begin
+    if (~reset_n) begin
+        counter <= 1;
+        new_clk <= 0;
+    end else if (counter >= HALF_RATIO) begin
         counter <= 1;
         new_clk <= ~new_clk;
     end else begin
