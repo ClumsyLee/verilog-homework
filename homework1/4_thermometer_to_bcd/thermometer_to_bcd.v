5ec17e6bdc57f7f38382a9185344f69eedeb2c54
Thomas Lee <lisihan969@gmail.com>
2015-05-06 02:32:38 +0800

    Fix behavior of t_to_bcd

--- homework1/4_thermometer_to_bcd/thermometer_to_bcd.v
+++ homework1/4_thermometer_to_bcd/thermometer_to_bcd.v
@@ -1,18 +1,24 @@
 module thermometer_to_bcd(bcd, thermometer);
 
-output [3:0] bcd;
+output [7:0] bcd;
 input [15:0] thermometer;
 
-assign bcd = (thermometer == 16'b0) ? 4'd0 :
-             (thermometer == 16'b1) ? 4'd1 :
-             (thermometer == 16'b11) ? 4'd2 :
-             (thermometer == 16'b111) ? 4'd3 :
-             (thermometer == 16'b1111) ? 4'd4 :
-             (thermometer == 16'b11111) ? 4'd5 :
-             (thermometer == 16'b111111) ? 4'd6 :
-             (thermometer == 16'b1111111) ? 4'd7 :
-             (thermometer == 16'b11111111) ? 4'd8 :
-             (thermometer == 16'b111111111) ? 4'd9 : 4'd0;
-
+assign bcd = (thermometer == 16'b0) ? 0 :
+             (thermometer == 16'b1) ? 1 :
+             (thermometer == 16'b11) ? 2 :
+             (thermometer == 16'b111) ? 3 :
+             (thermometer == 16'b1111) ? 4 :
+             (thermometer == 16'b11111) ? 5 :
+             (thermometer == 16'b111111) ? 6 :
+             (thermometer == 16'b1111111) ? 7 :
+             (thermometer == 16'b11111111) ? 8 :
+             (thermometer == 16'b111111111) ? 9 :
+             (thermometer == 16'b1111111111) ? 9 :
+             (thermometer == 16'b11111111111) ? {4'd1, 4'd0} :
+             (thermometer == 16'b111111111111) ? {4'd1, 4'd1} :
+             (thermometer == 16'b1111111111111) ? {4'd1, 4'd2} :
+             (thermometer == 16'b11111111111111) ? {4'd1, 4'd3} :
+             (thermometer == 16'b111111111111111) ? {4'd1, 4'd4} :
+             (thermometer == 16'b1111111111111111) ? {4'd1, 4'd5} : {4'd1, 4'd6};
 
 endmodule
--- homework1/4_thermometer_to_bcd/thermometer_to_bcd_tb.v
+++ homework1/4_thermometer_to_bcd/thermometer_to_bcd_tb.v
@@ -1,15 +1,15 @@
 module thermometer_to_bcd_tb;
 
-wire [3:0] bcd;
+wire [7:0] bcd;
 reg [15:0] thermometer = 0;
 
 thermometer_to_bcd translator(bcd, thermometer);
 
 initial begin
-    $monitor("thermometer: %b, bcd: %b, dec: %d",
-             thermometer, bcd, bcd);
+    $monitor("thermometer: %b, bcd: %b, dec: %d%d",
+             thermometer, bcd, bcd[7:4], bcd[3:0]);
     
-    repeat (9) #5 thermometer = {thermometer[14:0], 1'b1};
+    repeat (16) #5 thermometer = {thermometer[14:0], 1'b1};
 
     $finish;
 end
