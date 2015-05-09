eeba865814d0fdba9e5598bac14af0475074c795
Thomas Lee <lisihan969@gmail.com>
2015-05-09 12:32:18 +0800

    Use FSM to implement reset_n in freq_meter.v

--- exp3/freq_meter/freq_meter.v
+++ exp3/freq_meter/freq_meter.v
@@ -27,14 +27,28 @@ watchmaker #(100_000) scan_watchmaker(scan_clk, clk);
 // Central control.
 wire [15:0] num;
 reg [15:0] led_num;  // Number to be displayed on the LED.
-reg reset_n;
+wire reset_n;
 counter #(16, 9999) four_bit_counter(num, new_signal, 1'b1, reset_n);
 
-// Reset counter on control_clk
+
+reg [1:0] state = 0, next_state = 0;
+always @(state, control_clk) begin
+    case (state)
+        2'd0: next_state = (control_clk ? 2'd1 : 2'd0);
+        2'd1: next_state = 2;
+        2'd2: next_state = (control_clk ? 2'd2 : 2'd0);
+        default: next_state = 0;
+    endcase
+end
+
+assign reset_n = (state == 1 ? 1'b0 : 1'b1);
+
+always @(posedge clk) begin
+    state <= next_state;
+end
+
 always @(posedge control_clk) begin
     led_num <= num;
-    reset_n <= 0;
-    @(posedge clk) reset_n <= 1;
 end
 
 // Display the number
