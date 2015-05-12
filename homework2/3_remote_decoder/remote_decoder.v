90d7e10ba637b5c32ff31645b4ca88bd09e2406a
Thomas Lee <lisihan969@gmail.com>
2015-05-12 16:33:45 +0800

    Use only shift reg to implement h2/p3

--- homework2/3_remote_decoder/remote_decoder.v
+++ homework2/3_remote_decoder/remote_decoder.v
@@ -4,40 +4,16 @@ output [3:0] dout;
 output strobe;
 input din, clk;
 
-reg [5:0] shift_reg = 0;
-reg [2:0] state = 0, next_state = 0;
+reg [9:0] shift_reg = 0;
 
-localparam AWAIT = 0,
-           RECEIVE1 = 1,
-           RECEIVE2 = 2,
-           RECEIVE3 = 3,
-           RECEIVE4 = 4,
-           CHECK = 5,
-           OUTPUT = 6;
-
-// Calculate next state.
-always @(state, shift_reg) begin
-    case (state)
-        AWAIT:
-            next_state = (shift_reg[3:0] == 4'b0101 ? RECEIVE1 : AWAIT);
-        RECEIVE1, RECEIVE2, RECEIVE3, RECEIVE4:
-            next_state = next_state + 1'b1;
-        CHECK:
-            next_state = (^shift_reg[4:0] ? OUTPUT : AWAIT);
-        default:
-            next_state = AWAIT;
-    endcase
-end
-
-// State transfer.
-always @(posedge clk) begin
-    // Shift leftward
-    shift_reg <= {shift_reg[4:0], din};
-    state <= next_state;
-end
+always @(posedge clk)
+    shift_reg <= (strobe ? 10'b0 :                  // Clear.
+                           {shift_reg[8:0], din});  // Shift leftward.
 
 // Calculate output.
-assign strobe = (state == OUTPUT),
-       dout = (state == OUTPUT ? shift_reg[5:2] : 0);
+assign strobe = (shift_reg[9:6] == 4'b0101 &&  // Sync code.
+                 ^shift_reg[9:1]);             // Odd parity.
+
+assign dout = (strobe ? shift_reg[5:2] : 4'b0);
 
 endmodule
