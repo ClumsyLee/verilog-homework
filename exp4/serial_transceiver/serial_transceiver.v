7c6c266d2a4d1df13baa3bb86aaf4b51284b584f
Thomas Lee <lisihan969@gmail.com>
2015-05-20 21:15:24 +0800

    Build clocks

--- exp4/serial_transceiver/serial_transceiver.v
+++ exp4/serial_transceiver/serial_transceiver.v
@@ -3,6 +3,21 @@ module serial_transceiver(dout, din, clk);
 output dout;
 input din, clk;
 
+parameter BAUD_RATE = 9600;
+
+// If BAUD_RATE = 9600, then
+//     SAMPLE_CLK_RATIO = 651,
+//     WORK_CLK_RATIO = 10416,
+//     real baud rate = 9600.61,
+//     error = 0.00064%.
+localparam SAMPLE_CLK_RATIO = 100_000_000 / BAUD_RATE / 16,
+           WORK_CLK_RATIO = SAMPLE_CLK_RATIO * 16;
+
+// Build clocks.
+wire sample_clk, work_clk;
+watchmaker #(SAMPLE_CLK_RATIO) sample_watch(sample_clk, clk);
+watchmaker #(WORK_CLK_RATIO) work_watch(work_clk, clk);
+
 
 
 endmodule
