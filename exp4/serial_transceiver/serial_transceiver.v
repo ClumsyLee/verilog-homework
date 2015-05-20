d508173f67ffca8c9edb506968f5f12929c21fbd
Thomas Lee <lisihan969@gmail.com>
2015-05-20 22:37:34 +0800

    Make SAMPLE_RATIO a parameter

--- exp4/serial_transceiver/serial_transceiver.v
+++ exp4/serial_transceiver/serial_transceiver.v
@@ -3,15 +3,16 @@ module serial_transceiver(dout, din, clk);
 output dout;
 input din, clk;
 
-parameter BAUD_RATE = 9600;
+parameter BAUD_RATE = 9600,
+          SAMPLE_RATIO = 16;
 
 // If BAUD_RATE = 9600, then
 //     SAMPLE_CLK_RATIO = 651,
 //     WORK_CLK_RATIO = 10416,
 //     real baud rate = 9600.61,
 //     error = 0.00064%.
-localparam SAMPLE_CLK_RATIO = 100_000_000 / BAUD_RATE / 16,
-           WORK_CLK_RATIO = SAMPLE_CLK_RATIO * 16;
+localparam SAMPLE_CLK_RATIO = 100_000_000 / BAUD_RATE / SAMPLE_RATIO,
+           WORK_CLK_RATIO = SAMPLE_CLK_RATIO * SAMPLE_RATIO;
 
 // Build clocks.
 wire sample_clk, work_clk;
