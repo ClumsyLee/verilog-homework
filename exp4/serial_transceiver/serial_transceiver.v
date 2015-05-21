13053eb23b92f8caf90cd19bd3dbd9047b6aea6c
Thomas Lee <lisihan969@gmail.com>
2015-05-21 09:11:27 +0800

    Rename: WORK_CLK => SEND_CLK.

--- exp4/serial_transceiver/serial_transceiver.v
+++ exp4/serial_transceiver/serial_transceiver.v
@@ -8,16 +8,16 @@ parameter BAUD_RATE = 9600,
 
 // If BAUD_RATE = 9600, then
 //     SAMPLE_CLK_RATIO = 651,
-//     WORK_CLK_RATIO = 10416,
+//     SEND_CLK_RATIO = 10416,
 //     real baud rate = 9600.61,
 //     error = 0.00064%.
 localparam SAMPLE_CLK_RATIO = 100_000_000 / BAUD_RATE / SAMPLE_RATIO,
-           WORK_CLK_RATIO = SAMPLE_CLK_RATIO * SAMPLE_RATIO;
+           SEND_CLK_RATIO = SAMPLE_CLK_RATIO * SAMPLE_RATIO;
 
 // Build clocks.
-wire sample_clk, work_clk;
+wire sample_clk, send_clk;
 watchmaker #(SAMPLE_CLK_RATIO) sample_watch(sample_clk, clk);
-watchmaker #(WORK_CLK_RATIO) work_watch(work_clk, clk);
+watchmaker #(SEND_CLK_RATIO) send_watch(send_clk, clk);
 
 
 
