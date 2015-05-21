aa562f824eedb66980d8064bcea6b1ecf650cf62
Thomas Lee <lisihan969@gmail.com>
2015-05-21 19:44:28 +0800

    Add Makefile & test bench for exp4

---
+++ exp4/serial_transceiver/serial_transceiver_tb.v
@@ -0,0 +1,27 @@
+module serial_transceiver_tb;
+
+wire dout;
+reg din = 1, clk = 0;
+
+serial_transceiver transceiver(dout, din, clk);
+
+always #1 clk = ~clk;
+
+initial begin
+    $monitor($time, " dout: %b, din: %b", dout, din);
+
+    #10416 din = 0;
+    #10416 din = 1;
+    #10416 din = 1;
+    #10416 din = 0;
+    #10416 din = 0;
+    #10416 din = 0;
+    #10416 din = 1;
+    #10416 din = 1;
+    #10416 din = 0;
+    #10416 din = 1;
+
+    #400000 $finish;
+end
+
+endmodule
