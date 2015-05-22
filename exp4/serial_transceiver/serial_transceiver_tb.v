dc9aa1228f2711563fbe684f7f2bf20a7a7c293c
Thomas Lee <lisihan969@gmail.com>
2015-05-22 23:41:10 +0800

    Adjust timing in test bench

--- exp4/serial_transceiver/serial_transceiver_tb.v
+++ exp4/serial_transceiver/serial_transceiver_tb.v
@@ -10,7 +10,7 @@ always #1 clk = ~clk;
 initial begin
     $monitor($time, " dout: %b, din: %b", dout, din);
 
-    #800000 din = 0;
+    #300000 din = 0;
     #20832 din = 1;
     #20832 din = 1;
     #20832 din = 0;
@@ -21,7 +21,7 @@ initial begin
     #20832 din = 0;
     #20832 din = 1;
 
-    #800000 din = 0;
+    #80000 din = 0;
     #20832 din = 0;
     #20832 din = 1;
     #20832 din = 1;
