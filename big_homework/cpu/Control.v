871297033ed26ce4401ec133d075b74376a02d36
Thomas Lee <lisihan969@gmail.com>
2015-05-23 11:53:36 +0800

    Add init src files

---
+++ big_homework/cpu/Control.v
@@ -0,0 +1,34 @@
+
+module Control(OpCode, Funct,
+	PCSrc, Branch, RegWrite, RegDst, 
+	MemRead, MemWrite, MemtoReg, 
+	ALUSrc1, ALUSrc2, ExtOp, LuOp, ALUOp);
+	input [5:0] OpCode;
+	input [5:0] Funct;
+	output [1:0] PCSrc;
+	output Branch;
+	output RegWrite;
+	output [1:0] RegDst;
+	output MemRead;
+	output MemWrite;
+	output [1:0] MemtoReg;
+	output ALUSrc1;
+	output ALUSrc2;
+	output ExtOp;
+	output LuOp;
+	output [3:0] ALUOp;
+	
+	// Your code below
+	
+	// Your code above
+	
+	assign ALUOp[2:0] = 
+		(OpCode == 6'h00)? 3'b010: 
+		(OpCode == 6'h04)? 3'b001: 
+		(OpCode == 6'h0c)? 3'b100: 
+		(OpCode == 6'h0a || OpCode == 6'h0b)? 3'b101: 
+		3'b000;
+		
+	assign ALUOp[3] = OpCode[0];
+	
+endmodule
